7dae9f5908895234600800d14aa276cb05bfd58a
shept <github@sinortax.ru>
2025-03-25 16:54:39 +0300

    fix post fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import './App.css'
 
 function App() {
-  const [url, setUrl] = useState('')
-  const [result, setResult] = useState('')
+    const [url, setUrl] = useState('')
+    const [result, setResult] = useState('')
 
-  const onSubmit = async() => {
-    if (!url.trim()) return;
+    const onSubmit = async () => {
+        if (!url.trim()) return;
 
-    const response = await fetch('/gen', {method: "POST", body: JSON.stringify({"link": url})})
-    const data = await response.json()
-    setResult(data.uid)
+        const response = await fetch(
+            '/gen',
+            {
+                    method: "POST",
+                    body: JSON.stringify({"link": url}),
+                    headers: {'Content-Type': 'application/json'}
+                })
+        const data = await response.json()
+        setResult(data.uid)
 
-  }
-  return (
-    <>
-      <form onSubmit={_ => {_.preventDefault(); onSubmit()}}>
-        <input type="text" value={url} onChange={e => setUrl(e.target.value)}/>
-        <input type="submit" value="Сократить" />
-      </form>
-      <div className='result'>
-        <p>Сокращенный url: <span>{result}</span></p>
-      </div>
-    </>
-  )
+    }
+    return (
+        <>
+            <form onSubmit={_ => {
+                _.preventDefault();
+                onSubmit()
+            }}>
+                <input type="text" value={url} onChange={e => setUrl(e.target.value)}/>
+                <input type="submit" value="Сократить"/>
+            </form>
+            <div className='result'>
+                <p>Сокращенный url: <span>{result}</span></p>
+            </div>
+        </>
+    )
 }
 
 export default App
